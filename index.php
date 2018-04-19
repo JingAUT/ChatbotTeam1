3256134a87308b67390887cdf8da1a21cf18584d
JingAUT <ttn2100@autuni.ac.nz>
2018-04-19 15:09:24 +1200

    Second commit

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
- Volume in drive C is Local Disk
- Volume Serial Number is ACEE-5A86
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Home Page</title>
+<style>
+  p.small{
+    line-height: 0.5;
+  }
+</style>
+</head>
+<body>
+    <h1>Status Posting System</h1>
+    <p class="small">Name: Jing Zhang<br></br>Student ID: 17956190<br></br>Email: ttn2100@autuni.ac.nz</p>
+    <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source</p>
+    <p class="small">
+      <a href="poststatusform.php">Post a new status</a><br></br>
+      <a href="searchstatusform.html">Search status</a>
+      <a href="about.html" style="float:right">About this assignment</a>
+    </p>
+</form>
+</body>
+</html>
 
- Directory of C:\Users\13742\Desktop\Chatbot
-
-19/04/2018  02:34 PM    <DIR>          .
-19/04/2018  02:34 PM    <DIR>          ..
-19/04/2018  02:34 PM                 0 index.php
-               1 File(s)              0 bytes
-               2 Dir(s)  135,869,771,776 bytes free
