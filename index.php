0fd092c021e5cad1504cd821d2b575c48592d616
JingAUT <ttn2100@autuni.ac.nz>
2018-04-19 14:32:25 +1200

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+ Volume in drive C is Local Disk
+ Volume Serial Number is ACEE-5A86
+
+ Directory of C:\Users\13742\Desktop\Chatbot
+
+19/04/2018  02:31 PM    <DIR>          .
+19/04/2018  02:31 PM    <DIR>          ..
+19/04/2018  02:31 PM                 0 index.php
+               1 File(s)              0 bytes
+               2 Dir(s)  135,869,677,568 bytes free
