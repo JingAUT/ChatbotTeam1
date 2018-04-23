f0fc832c13d1ee39e41f0be74d41a9854cf46e9e
JingAUT <ttn2100@autuni.ac.nz>
2018-04-23 18:09:12 +1200

    commit

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+
 	$challenge = $_REQUEST['hub_challenge'];
 	$verify_token = $_REQUEST['hub_verify_token'];
 	// Set this Verify Token Value on your Facebook App 
@@ -33,6 +35,36 @@
 	if(!empty($input['entry'][0]['messaging'][0]['message'])){
 	  $result = curl_exec($ch);
 	}
+	
+	use Mpociot\BotMan\BotManFactory;
+	use Mpociot\BotMan\BotMan;
+	use BotMan\BotMan\Drivers\DriverManager;
+
+	$config = [
+		'facebook' => [
+		'token' => 'EAAMo0pOX7e0BAMzEG1LrKHRkb1LWPo2pO7wuB4lfyE1mNAKUzWhEMyHwGHCbeBSnaqIMjuxKJKlNGyoWWBcBEmaNqH0TT2PGJVprirJZCfUOSqjPRcHstqml5vS05jRTeFEM0WAp4RmBHJ6NvWVHemarIeGG6PcdMf8iZAjgZDZD',
+		'app_secret' => '48c0417c72d6ff6aa0ea81067b366a9f',
+		'verification'=>'MY_SECRET_VERIFICATION_TOKEN',
+		]
+	];
+
+	// Load the driver(s) you want to use
+	DriverManager::loadDriver(\BotMan\Drivers\Telegram\TelegramDriver::class);
+
+	// Create an instance
+	$botman = BotManFactory::create($config);
+
+	// Give the bot something to listen for.
+	$botman->hears('hello', function (BotMan $bot) {
+		$bot->reply('Hello yourself.');
+	});
+
+	// Start listening
+	$botman->listen();
+	
+
+);
+	
 ?>
 
 
