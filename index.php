9f33eab84aa8d91e0b0f68b95a2403a4658810da
JingAUT <ttn2100@autuni.ac.nz>
2018-04-23 17:16:22 +1200

    commit

--- index.php
+++ index.php
@@ -35,19 +35,4 @@
 	}
 ?>
 
-<html>
-<head>
-<meta http-equiv="content-type" />
-<title>Home Page</title>
-</head>
-<body>
-    <h1>Chatbot</h1>
-    
-    <p>This is a test for heroku</p>
-    <p>This is a test for github</p>
-    
-    
-</form>
-</body>
-</html>
 
