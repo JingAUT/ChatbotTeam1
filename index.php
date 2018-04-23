5f9878dcfdc35e5a1e9d8517ec4ad5b6f95034b4
JingAUT <ttn2100@autuni.ac.nz>
2018-04-23 18:30:55 +1200

    commit

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 
 	$config = [
 		"facebook" => [
-		'token' => 'EAAMo0pOX7e0BAMzEG1LrKHRkb1LWPo2pO7wuB4lfyE1mNAKUzWhEMyHwGHCbeBSnaqIMjuxKJKlNGyoWWBcBEmaNqH0TT2PGJVprirJZCfUOSqjPRcHstqml5vS05jRTeFEM0WAp4RmBHJ6NvWVHemarIeGG6PcdMf8iZAjgZDZD',
-		'app_secret' => '48c0417c72d6ff6aa0ea81067b366a9f',
-		'verification'=>'MY_SECRET_VERIFICATION_TOKEN',
+		"token" => "EAAMo0pOX7e0BAMzEG1LrKHRkb1LWPo2pO7wuB4lfyE1mNAKUzWhEMyHwGHCbeBSnaqIMjuxKJKlNGyoWWBcBEmaNqH0TT2PGJVprirJZCfUOSqjPRcHstqml5vS05jRTeFEM0WAp4RmBHJ6NvWVHemarIeGG6PcdMf8iZAjgZDZD",
+		"app_secret" => "48c0417c72d6ff6aa0ea81067b366a9f",
+		"verification"=>"oihoigghu8768598gigfu5rfytfuyf",
 		]
 	];
 
 	// Load the driver(s) you want to use
-	DriverManager::loadDriver(\BotMan\Drivers\Telegram\TelegramDriver::class);
+	DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
 
 	// Create an instance
 	$botman = BotManFactory::create($config);
