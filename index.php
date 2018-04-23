7c2325d18e686f88dbe6c26c947965a03ec1d1dc
JingAUT <ttn2100@autuni.ac.nz>
2018-04-23 18:41:51 +1200

    commit

--- index.php
+++ index.php
@@ -6,19 +6,19 @@
 	use BotMan\BotMan\Drivers\DriverManager;
 
 	$config = [
-		"facebook" => [
-		"token" => "EAAMo0pOX7e0BAMzEG1LrKHRkb1LWPo2pO7wuB4lfyE1mNAKUzWhEMyHwGHCbeBSnaqIMjuxKJKlNGyoWWBcBEmaNqH0TT2PGJVprirJZCfUOSqjPRcHstqml5vS05jRTeFEM0WAp4RmBHJ6NvWVHemarIeGG6PcdMf8iZAjgZDZD",
-		"app_secret" => "48c0417c72d6ff6aa0ea81067b366a9f",
-		"verification"=>"oihoigghu8768598gigfu5rfytfuyf",
-		]
+		'hipchat_urls'=>[
+			'YOUR-INTEGRATION-URL-1',
+			'YOUR-INTEGRATION-URL-2',
+		],
+		'facebook_token' => 'EAAMo0pOX7e0BAMzEG1LrKHRkb1LWPo2pO7wuB4lfyE1mNAKUzWhEMyHwGHCbeBSnaqIMjuxKJKlNGyoWWBcBEmaNqH0TT2PGJVprirJZCfUOSqjPRcHstqml5vS05jRTeFEM0WAp4RmBHJ6NvWVHemarIeGG6PcdMf8iZAjgZDZD',
+		'facebook_app_secret' => '48c0417c72d6ff6aa0ea81067b366a9f',	
 	];
-
-	// Load the driver(s) you want to use
-	DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
 
 	// Create an instance
 	$botman = BotManFactory::create($config);
 
+	$botman -> verifyServices('skfgliwuehrqw342639o5345');
+	
 	// Give the bot something to listen for.
 	$botman->hears('hello', function (BotMan $bot) {
 		$bot->reply('Hello yourself.');
