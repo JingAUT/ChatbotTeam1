9d9c01c0b361810d158b1aee6810be3ceb89d20a
Beau123123 <liyuanyu3@gmail.com>
2018-05-20 10:25:41 +1200

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 <title>Chat bot team 1</title>
 </head>
 <body>
-    { font-family:Verdana; font-size:12px; margin:0 auto;}
-
-#container {margin: 0 auto; width:1060px;}
     <script>
       window.fbAsyncInit = function() {
         FB.init({
@@ -30,18 +27,15 @@
       <div id="u0" class="ax_default heading_1">
         <div id="u0_div" class=""></div>
         <div id="u0_text" class="text ">
-            <div id="box">
           <h1><span style="font-family:'Gill Sans Ultra Bold Oblique', 'Gill Sans';font-weight:800;font-style:oblique;color:#FF9933;">Course&nbsp; Query</span><span style="font-family:'Arial Negreta', 'Arial Normal', 'Arial';font-weight:700;font-style:normal;">&nbsp; </span></h1>
         </div>
       </div>
-          </div>
-     </div>
     <iframe
     width="350"
     height="430"
     src="https://console.dialogflow.com/api-client/demo/embedded/f0a15ccf-31f6-4105-9e47-08047a7ce001">
     </iframe>
-<p>Please contact us through Messenger </p>
+<p><center>Please contact us through Messenger </center></p>
     <div class="fb-messengermessageus" 
           messenger_app_id="889309814582765" 
           page_id="980748105428133"
