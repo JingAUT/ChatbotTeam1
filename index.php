d7bb7dbe40c03dbc5a2edc4311e1f471dbd333e3
Beau123123 <liyuanyu3@gmail.com>
2018-05-20 10:27:43 +1200

    Update index.php

--- index.php
+++ index.php
@@ -27,21 +27,21 @@
       <div id="u0" class="ax_default heading_1">
         <div id="u0_div" class=""></div>
         <div id="u0_text" class="text ">
-          <h1><span style="font-family:'Gill Sans Ultra Bold Oblique', 'Gill Sans';font-weight:800;font-style:oblique;color:#FF9933;">Course&nbsp; Query</span><span style="font-family:'Arial Negreta', 'Arial Normal', 'Arial';font-weight:700;font-style:normal;">&nbsp; </span></h1>
+          <h1><center><span style="font-family:'Gill Sans Ultra Bold Oblique', 'Gill Sans';font-weight:800;font-style:oblique;color:#FF9933;">Course&nbsp; Query</span><span style="font-family:'Arial Negreta', 'Arial Normal', 'Arial';font-weight:700;font-style:normal;">&nbsp; </span></center></h1>
         </div>
       </div>
-    <iframe
+    <center><iframe
     width="350"
     height="430"
     src="https://console.dialogflow.com/api-client/demo/embedded/f0a15ccf-31f6-4105-9e47-08047a7ce001">
-    </iframe>
+    </iframe></center>
 <p><center>Please contact us through Messenger </center></p>
-    <div class="fb-messengermessageus" 
+    <center><div class="fb-messengermessageus" 
           messenger_app_id="889309814582765" 
           page_id="980748105428133"
           color="white"
           size="large">
-        </div>  
+        </div>  </center>
 </body>
 </html>
 
