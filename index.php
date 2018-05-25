a57210f64f7f8fc138b3415148a680bf38b1fb19
JingAUT <30999349+JingAUT@users.noreply.github.com>
2018-05-26 10:07:26 +1200

    Update index.php

--- index.php
+++ index.php
@@ -30,18 +30,17 @@
           <h1><center><span style="font-family:'Gill Sans Ultra Bold Oblique', 'Gill Sans';font-weight:800;font-style:oblique;color:#FF9933;">Course&nbsp; Bot</span><span style="font-family:'Arial Negreta', 'Arial Normal', 'Arial';font-weight:700;font-style:normal;">&nbsp; </span></center></h1>
         </div>
       </div>
-    <center><iframe
-    width="350"
-    height="430"
-    src="https://console.dialogflow.com/api-client/demo/embedded/f0a15ccf-31f6-4105-9e47-08047a7ce001">
-    </iframe></center>
-<p><center>Please contact us through Messenger </center></p>
-    <center><div class="fb-messengermessageus" 
+    
+    <center><iframe width="350" height="430" src="https://console.dialogflow.com/api-client/demo/embedded/f0a15ccf-31f6-4105-9e47-08047a7ce001"></iframe></center>
+    <p><center>Please contact us through Messenger </center></p>
+    <center>
+        <div class="fb-messengermessageus" 
           messenger_app_id="889309814582765" 
           page_id="980748105428133"
           color="white"
           size="large">
-        </div>  </center>
+        </div>  
+    </center>
 </body>
 </html>
 
